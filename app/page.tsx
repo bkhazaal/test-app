972b8912ae6471e15bb304bf693f98d02f77c283
Ben Khazaal <thajukez101@gmail.com>
2024-01-10 16:02:12 -0700

    More Test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-right size-auto justify-between p-24">
-      <h1 className="text-center">Testing</h1>
+    <main className="max-w-[1200px] mx-auto">
+      <div className="h-[230px] text-[120px] text-center">BEN.COM</div>
+      <div className="h-[100px] text-[30px] text-center">
+        I made this thing...
+      </div>
+      <div className="h-[100px] text-[25px] text-center">Just now...</div>
+      <div className="h-[100px] text-[20px] text-center">Because I can...</div>
+      <div className="h-[100px] text-[15px] text-center">
+        Aren't you proud of me?
+      </div>
     </main>
   );
 }
