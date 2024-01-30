59d7ab874872b7da242b4009edb5b1169818230b
Ben Khazaal <thajukez101@gmail.com>
2024-01-30 16:06:27 -0700

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,26 @@
 export default function Home() {
   return (
-    <main className="max-w-[1200px] mx-auto">
-      <div className="h-[230px] text-[120px] text-center">BEN.COM</div>
-      <div className="h-[100px] text-[30px] text-center">
-        I made this thing...
+    <main className="max-w-[1200px] mx-auto text-black ">
+      <nav>
+        <div></div>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+      <div className="py-10">
+        <div>
+          <h1 className="text-5xl text-center ">
+            Workouts Made <br />{" "}
+            <span className="text-emerald-500">Exclusively</span> For You
+          </h1>
+          <p></p>
+          <button></button>
+        </div>
+        <div></div>
       </div>
-      <div className="h-[100px] text-[25px] text-center">Just now...</div>
-      <div className="h-[100px] text-[20px] text-center">Because I can...</div>
-      <div className="h-[100px] text-[15px] text-center">
-        Arent you proud of me?
-      </div>
+      <footer></footer>
     </main>
   );
 }
